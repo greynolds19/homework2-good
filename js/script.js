e670bc479c76024a8a6b7718ca814dae97880072
Gary Reynolds <garyreynolds@Garys-MacBook-Pro.local>
2014-11-04 20:59:05 -0500

    committing the better version of hw2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$("#Messi").click(function(){
+      $("#names-here").text("You picked Lionel Messi");
+});
+
+$("#Ronaldo").click(function(){
+      $("#names-here").text("You picked Christiano Ronaldo");
+});
+
+
+
